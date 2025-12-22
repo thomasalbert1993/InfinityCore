1336049d6c28b9ef8d9455277436ad071f818f6a
Thomas Albert <thomas@crewmate.fr>
2025-12-22 18:40:10 +0100

    Add Identifiable helpers

---
+++ Sources/CrewmateCore/Identifiable.swift
@@ -0,0 +1,53 @@
+//
+//  Identifiable.swift
+//  CrewmateCore
+//
+//  Created by Thomas ALBERT on 22/12/2025.
+//
+
+import Foundation
+
+extension Collection where Element: Identifiable {
+    
+    /// The elements identifiers.
+    public var ids: [Element.ID] {
+        map { $0.id }
+    }
+    
+    /// Gets the first element matching a given identifier.
+    ///
+    /// - Parameter id: The identifier to fetch.
+    ///
+    /// - Returns: The first element with this identifier.
+    public func first(withID id: Element.ID) -> Element? {
+        first { $0.id == id }
+    }
+    
+    /// Gets the index of the first element matching a given identifier.
+    ///
+    /// - Parameter id: The identifier to match.
+    ///
+    /// - Returns: The index of the first element with this identifier.
+    public func firstIndex(withID id: Element.ID) -> Index? {
+        firstIndex { $0.id == id }
+    }
+    
+    /// Checs whether collection contains an element with a given identifier.
+    ///
+    /// - Parameter id: The identifier to match.
+    ///
+    /// - Returns: A boolean indicating whether there is at least one element with this identifier.
+    public func containsID(_ id: Element.ID) -> Bool {
+        contains { $0.id == id }
+    }
+}
+
+extension Array where Element: Identifiable {
+    
+    /// Removes all elements matching a given identifier.
+    ///
+    /// - Parameter id: The identifier to match.
+    public mutating func removeAll(withID id: Element.ID) {
+        removeAll { $0.id == id }
+    }
+}
