f81aa6d11037384b662fb940d70e6ac5fcecad9b
Thomas Albert <thomas@crewmate.fr>
2025-12-22 18:57:27 +0100

    Add Dictionary and Collection helpers

---
+++ Sources/CrewmateCore/Extensions/Collection.swift
@@ -0,0 +1,16 @@
+//
+//  Collection.swift
+//  CrewmateCore
+//
+//  Created by Thomas ALBERT on 22/12/2025.
+//
+
+import Foundation
+
+extension Collection {
+    
+    /// Returns `nil` when collection is empty.
+    public var emptyAsNil: Self? {
+        isEmpty ? nil : self
+    }
+}
---
+++ Sources/CrewmateCore/Extensions/Dictionary.swift
@@ -0,0 +1,40 @@
+//
+//  Dictionary.swift
+//  CrewmateCore
+//
+//  Created by Thomas ALBERT on 22/12/2025.
+//
+
+import Foundation
+
+extension Dictionary {
+    
+    /// Maps keys using a given transform.
+    ///
+    /// - Parameter transform: The transform to apply to keys.
+    ///
+    /// - Returns: The mapped dictionary.
+    public func mapKeys<T>(_ transform: (Key) throws -> T) rethrows -> [T:Value] {
+        var dict = [T:Value]()
+        for (key, value) in self {
+            let newKey = try transform(key)
+            dict[newKey] = value
+        }
+        return dict
+    }
+    
+    /// Maps keys using a given transform. Keys are omitted when transform returns `nil`.
+    ///
+    /// - Parameter transform: The transform to apply to keys.
+    ///
+    /// - Returns: The mapped dictionary.
+    public func compactMapKeys<T>(_ transform: (Key) throws -> T?) rethrows -> [T:Value] {
+        var dict = [T:Value]()
+        for (key, value) in self {
+            if let newKey = try transform(key) {
+                dict[newKey] = value
+            }
+        }
+        return dict
+    }
+}
