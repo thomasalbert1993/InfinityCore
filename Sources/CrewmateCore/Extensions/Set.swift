3b19e13088e2b495d43b8eeea2c2e8450506bed9
Thomas Albert <thomas@crewmate.fr>
2025-12-23 10:06:51 +0100

    Add String helpers

---
+++ Sources/CrewmateCore/Extensions/Set.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  CrewmateCore
+//
+//  Created by Thomas ALBERT on 23/12/2025.
+//
+
+import Foundation
--- Sources/CrewmateCore/Extensions/String.swift
+++ Sources/CrewmateCore/Extensions/String.swift
@@ -9,9 +9,24 @@ import Foundation
 
 extension String {
     
+    /// Trims whitespaces from string.
+    public func trimmed() -> String {
+        trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    /// Removes accents from string.
+    public func removingAccents() -> String {
+        folding(options: .diacriticInsensitive, locale: .current)
+    }
+    
     /// Converts an UTC ISO8601 formatted date into a `Date`.
     /// Both second and microsecond precision are allowed.
     public func toDate() throws -> Date {
         try ISO8601DateFormatter.date(from: self)
+    }
+    
+    /// Converts string to an `URL`.
+    public func toURL() throws -> URL {
+        try URL(string: trimmed()) ?! "Invalid URL: \(self)"
     }
 }
