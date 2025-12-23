810208d88117d6e67642c207956d2ac018907a3e
Thomas Albert <thomas@crewmate.fr>
2025-12-23 09:54:05 +0100

    Add Date helpers

--- Sources/CrewmateCore/Extensions/Date.swift
+++ Sources/CrewmateCore/Extensions/Date.swift
@@ -9,6 +9,24 @@ import Foundation
 
 extension Date {
     
+    /// Gets the date components (year/month/day).
+    ///
+    /// - Parameter includingTime: A boolean indicating wheter time components (hours/minutes/seconds) should be included.
+    /// - Parameter calendar: The calendar to use.
+    ///
+    /// - Returns: The `DateComponents`.
+    public func components(includingTime: Bool = true, in calender: Calendar = .current) -> DateComponents {
+        let calendar = Calendar.current
+        var components: DateComponents
+        if includingTime {
+            components = calendar.dateComponents([ .year, .month, .day, .hour, .minute, .second ], from: self)
+        } else {
+            components = calendar.dateComponents([ .year, .month, .day ], from: self)
+        }
+        components.calendar = calendar
+        return components
+    }
+    
     public enum TimeStyle {
         case none
         case second
@@ -20,7 +38,7 @@ extension Date {
     /// - Parameter timeStyle: The time style (ie. `none`, `second` or `microsecond`).
     ///
     /// - Returns: The serialized date.
-    public func toUTCString(timeStyle: TimeStyle = .second) -> String {
+    public func utcString(timeStyle: TimeStyle = .second) -> String {
         switch timeStyle {
             case .none:
                 return ISO8601DateFormatter.dateFormatter.string(from: self)
