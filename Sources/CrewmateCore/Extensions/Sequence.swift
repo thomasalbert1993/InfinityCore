0e2f786b37eb2bd228b22e741f4daf2c48b7b7e2
Thomas Albert <thomas@crewmate.fr>
2026-01-13 17:44:51 +0800

    Add Sequence helpers

---
+++ Sources/CrewmateCore/Extensions/Sequence.swift
@@ -0,0 +1,22 @@
+//
+//  Sequence.swift
+//  CrewmateCore
+//
+//  Created by Thomas ALBERT on 13/01/2026.
+//
+
+import Foundation
+
+extension Sequence {
+    
+    /// Groups elements using a given keypath.
+    ///
+    /// - Parameter keyPath: The keypath for grouping.
+    ///
+    /// - Returns: The grouped elements.
+    public func grouped<Key: Hashable>(by keyPath: KeyPath<Element,Key>) -> [Key:[Element]] {
+        .init(grouping: self) {
+            $0[keyPath: keyPath]
+        }
+    }
+}
