d67301e09d47ab5b8aa2cc685eef968069b9f06d
Thomas Albert <thomas@crewmate.fr>
2026-01-15 10:30:09 +0800

    Update min version to iOS 16

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CrewmateCore",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
         .macOS(.v13),
         .tvOS(.v12),
     ],
