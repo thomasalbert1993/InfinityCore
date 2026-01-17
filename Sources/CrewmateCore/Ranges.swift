4932c66a4398300b6612ff2a76ed996e3a16413b
Thomas Albert <thomas@crewmate.fr>
2026-01-17 14:40:17 +0800

    Rewrite nullable bounds ClosedRange with RangeBound protocol to be more generic

--- Sources/CrewmateCore/Ranges.swift
+++ Sources/CrewmateCore/Ranges.swift
@@ -7,50 +7,61 @@
 
 import Foundation
 
-extension ClosedRange<Double> {
+public protocol RangeBound: Comparable, Sendable {
+    static var minApplicableValue: Self { get }
+    static var maxApplicableValue: Self { get }
+}
+
+extension Int: RangeBound {
+    public static var minApplicableValue: Int { .min }
+    public static var maxApplicableValue: Int { .max }
+}
+
+extension UInt: RangeBound {
+    public static var minApplicableValue: UInt { .min }
+    public static var maxApplicableValue: UInt { .max }
+}
+
+extension Double: RangeBound {
+    public static var minApplicableValue: Double { -.greatestFiniteMagnitude }
+    public static var maxApplicableValue: Double { .greatestFiniteMagnitude }
+}
+
+extension Float: RangeBound {
+    public static var minApplicableValue: Float { -.greatestFiniteMagnitude }
+    public static var maxApplicableValue: Float { .greatestFiniteMagnitude }
+}
+
+extension Date: RangeBound {
+    public static var minApplicableValue: Date { .distantPast }
+    public static var maxApplicableValue: Date { .distantFuture }
+}
+
+extension ClosedRange where Bound: RangeBound {
     
     /// The nullable lower bound (`nil` when equals to min appliable value).
-    public var nullableLowerBound: Double? {
-        lowerBound != -.greatestFiniteMagnitude ? lowerBound : nil
+    public var nullableLowerBound: Bound? {
+        lowerBound != Bound.minApplicableValue ? lowerBound : nil
     }
     
     /// The nullable upper bound (`nil` when equals to max appliable value).
-    public var nullableUpperBound: Double? {
-        upperBound != .greatestFiniteMagnitude ? upperBound : nil
-    }
-    
-    /// Checking wether two ranges are intersecting.
-    ///
-    /// - Parameter other: The range to compare.
-    ///
-    /// - Returns: A boolean indicating wether the two ranges are intersecting.
-    public func intersects(with other: Self) -> Bool {
-        lowerBound <= other.upperBound && upperBound >= other.lowerBound
+    public var nullableUpperBound: Bound? {
+        upperBound != Bound.maxApplicableValue ? upperBound : nil
     }
     
     /// Initializing from nullable lower and upper bounds.
     ///
     /// - Parameter nullableLowerBound: The lower bound.
     /// - Parameter nullableUpperBound: The upper bound.
-    public init(nullableLowerBound: Double?, nullableUpperBound: Double?) {
+    public init(nullableLowerBound: Bound?, nullableUpperBound: Bound?) {
         self.init(uncheckedBounds: (
-            lower: nullableLowerBound ?? -.greatestFiniteMagnitude,
-            upper: nullableUpperBound ?? .greatestFiniteMagnitude
+            lower: nullableLowerBound ?? Bound.minApplicableValue,
+            upper: nullableUpperBound ?? Bound.maxApplicableValue
         ))
     }
 }
 
-extension ClosedRange<Int> {
-    
-    /// The nullable lower bound (`nil` when equals to min appliable value).
-    public var nullableLowerBound: Int? {
-        lowerBound != .min ? lowerBound : nil
-    }
-    
-    /// The nullable upper bound (`nil` when equals to max appliable value).
-    public var nullableUpperBound: Int? {
-        upperBound != .max ? upperBound : nil
-    }
+extension ClosedRange {
     
     /// Checking wether two ranges are intersecting.
     ///
@@ -59,16 +70,5 @@ extension ClosedRange<Int> {
     /// - Returns: A boolean indicating wether the two ranges are intersecting.
     public func intersects(with other: Self) -> Bool {
         lowerBound <= other.upperBound && upperBound >= other.lowerBound
-    }
-    
-    /// Initializing from nullable lower and upper bounds.
-    ///
-    /// - Parameter nullableLowerBound: The lower bound.
-    /// - Parameter nullableUpperBound: The upper bound.
-    public init(nullableLowerBound: Int?, nullableUpperBound: Int?) {
-        self.init(uncheckedBounds: (
-            lower: nullableLowerBound ?? .min,
-            upper: nullableUpperBound ?? .max
-        ))
     }
 }
